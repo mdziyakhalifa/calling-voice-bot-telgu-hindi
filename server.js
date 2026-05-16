8fdbac6dd6ff88d40523da9228a6c5899f61b3cf
mohammad ziya khalifa <mohammadziyakhalifa@gmail.com>
2026-05-16 17:55:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const logFilePath = path.join(__dirname, 'chat_logs.txt');
 
